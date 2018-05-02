96360d3e9881d2a34cbbaa1fdf7e80a38897b8f5
anton-logom <anton-logom@yndex.ru>
2018-05-02 22:49:01 +0300

    обновлен вывод из базы данных

--- mysql_print.py
+++ mysql_print.py
@@ -1,3 +1,5 @@
+# coding: utf8
+
 import MySQLdb
 
 # НАСТРОЙКИ
@@ -5,19 +7,23 @@ import mysql_config
 
 
 def print_posts(lst):
+    print(lst)
+    lastpost = 0
     for i in range(0, len(lst)):
-        print("Пост №" + str(i + 1))
-        print("ID поста:" + lst[i][1], end='\n')
-        print("Автор и текст:")
-        print(lst[i][2], end='\n')
-        print(lst[i][3], end='\n')
-        if len(lst[i][5]) > 1:
-            print("Изображения:")
+        if not(lastpost == lst[i][0]):
+            print('---------------')
+            print("Пост №" + str(lst[i][0]))
+            print("ID поста:" + lst[i][1], end='\n')
+            print("Автор и текст:")
+            print(lst[i][2], end='\n')
+            print(lst[i][3], end='\n')
+        lastpost = lst[i][0]
+        if (len(lst[i][5]) > 1) & (not(lst[i][5] == 'Изображения отсутсвуют.')):
+            print("Изображение:")
             print(lst[i][5], end='\n')
-        if len(lst[i][4]) > 1:
-            print("Внешние ссылки:")
-            print(lst[i][4], end='\n')
-        print('---------------')
+        if (len(lst[i][7]) > 1) & (not(lst[i][7] == 'Ссылки отсутсвуют.')):
+            print("Внешняя ссылка:")
+            print(lst[i][7], end='\n')
 
 
 if __name__ == '__main__':
@@ -29,7 +35,8 @@ if __name__ == '__main__':
                            charset="utf8",
                            db=mysql_config.db)
     c = conn.cursor()
-    c.execute('SELECT * FROM `parse`')
+    c.execute('SELECT * FROM parse_text JOIN parse_images ON (parse_text.id = parse_images.id) JOIN parse_links ON (parse_text.id = parse_links.id)')
     c_list = c.fetchall()
     conn.close()
     print_posts(c_list)
+    input()
